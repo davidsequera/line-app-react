0fe970f5cfdb8ac627a72c1002cedc72501a4b75
David Sequera <davidsequerads@gmail.com>
2020-04-13 00:24:54 -0500

    Cambio de ext a JSX

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -9,16 +9,13 @@ class RestaurantList extends React.Component{
         return(
             <React.Fragment>
                 <ul className="RestaurantList_container">
-                    {this.props.restaurant.map((restaurant) =>{
-                        return(
-                            <li key={restaurant.id}>
+                    {this.props.restaurant.map((restaurant) => <li key={restaurant.id}>
                                 <Link to={`explore/${restaurant.Name}/places`}className="RestaurantList_item">
                                     <img className="RestaurantList_logo" src={restaurant.imgUrl} alt="logo restaurant"></img>
                                     <h2 className="RestaurantList_Name">{restaurant.Name}</h2>
                                 </Link>
                             </li>
-                        )
-                    })}
+                    )}
                 </ul>
             </React.Fragment>
         )
